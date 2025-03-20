a6f3b31f2bc276283121e7c493a14b9ced0ff80b
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-03-20 17:55:22 +0000

    test: update.

--- src/test/readme.md.spec.ts
+++ src/test/readme.md.spec.ts
@@ -1,5 +1,9 @@
 // Class.
-import { History as AbstractHistory } from "../lib";
+import {
+  History as AbstractHistory,
+  HistoryAppend as AbstractHistoryAppend,
+  HistoryPrepend as AbstractHistoryPrepend
+} from "../lib";
 
 // Initialize.
 const history = new class History<Type, Size extends number = number> extends AbstractHistory<Type, Size>{}({value: 5, size: 5});
@@ -40,5 +44,49 @@ console.log(`redo`, 20);
 history.redo();
 
 console.log(history.undoHistory.get(), history.current, history.redoHistory.get()); // [10, 15], 20, [25, 30, 35]
+
+console.groupEnd();
+
+
+
+console.group(`HistoryAppend`);
+
+export const historyAppend = new class HistoryAppend<Type = number, Size extends number = number>
+  extends AbstractHistoryAppend<Type, Size>{}();
+
+// Add to the history.
+console.log(`add`, 127, 227);
+historyAppend.add(127).add(227);
+console.log(historyAppend.get()); // [127, 227]
+
+// Peek.
+console.log(`peekLast()`, historyAppend.peekLast()); // Outputs: 127
+console.log(`peekNext()`, historyAppend.peekNext()); // Outputs: 227
+
+// Take from the history.
+console.log(historyAppend.take()); // 227
+console.log(historyAppend.get()); // [127]
+
+console.groupEnd();
+
+
+
+console.group(`HistoryPrepend`);
+
+export const historyPrepend = new class HistoryPrepend<Type = number, Size extends number = number>
+  extends AbstractHistoryPrepend<Type, Size>{}();
+
+// Add to the history.
+console.log(`add`, 127, 327, 227);
+historyPrepend.add(127).add(327).add(227);
+console.log(historyPrepend.get()); // [227, 327, 127]
+
+// Peek.
+console.log(`peekLast()`, historyPrepend.peekLast()); // Outputs: 127
+console.log(`peekNext()`, historyPrepend.peekNext()); // Outputs: 227
+
+// Take from the history.
+console.log(historyPrepend.take()); // 227
+console.log(historyPrepend.get()); // [327, 127]
 
 console.groupEnd();
