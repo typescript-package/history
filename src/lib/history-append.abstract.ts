99aba95b04c8d06cdbe3829e99fdd8e0f817810c
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-04-08 22:47:17 +0000

    refactor(HistoryAppend): change `Type` to `Value`, and `Storage` to `DataType`.

--- src/lib/history-append.abstract.ts
+++ src/lib/history-append.abstract.ts
@@ -13,16 +13,16 @@ import { HistoryCore } from './history-core.abstract';
  * @export
  * @abstract
  * @class HistoryAppend
- * @template Type 
+ * @template Value 
  * @template {number} [Size=number] 
- * @template {DataCore<Type[]>} [Storage=Data<Type[]>] 
- * @extends {HistoryCore<Type, Size, Storage>}
+ * @template {DataCore<Value[]>} [DataType=Data<Value[]>] 
+ * @extends {HistoryCore<Value, Size, DataType>}
  */
 export abstract class HistoryAppend<
-  Type,
+  Value,
   Size extends number = number,
-  Storage extends DataCore<Type[]> = Data<Type[]>
-> extends HistoryCore<Type, Size, Storage> {
+  DataType extends DataCore<Value[]> = Data<Value[]>
+> extends HistoryCore<Value, Size, DataType> {
   /**
    * @description The default value of maximum history size.
    * @public
@@ -45,21 +45,22 @@ export abstract class HistoryAppend<
    * Creates an instance of `HistoryAppend` child class.
    * @constructor
    * @param {Size} [size=HistoryAppend.size as Size] 
+   * @param {new (value: Value[]) => DataType} [data=Data as any] 
    */
   constructor(
     size: Size = HistoryAppend.size as Size,
-    storage: new (value: Type[]) => Storage = Data as any
+    data: new (value: Value[]) => DataType = Data as any
   ) {
-    super(size, storage);
+    super(size, data);
   }
 
   /**
    * @description Adds the value to the history.
    * @public
-   * @param {Type} value The value to store.
+   * @param {Value} value The value to store.
    * @returns {this} The current instance.
    */
-  public add(value: Type): this {
+  public add(value: Value): this {
     const history = super.history;
     history.length >= super.size && history.shift();
     super.size > 0 && history.push(value);
@@ -69,27 +70,27 @@ export abstract class HistoryAppend<
   /**
    * @description Returns the last value that would be use to redo or undo without modifying history.
    * @public
-   * @returns {Type | undefined} The next redo value.
+   * @returns {Value | undefined} The next redo value.
    */
-  public peekLast(): Type | undefined {
+  public peekLast(): Value | undefined {
     return super.history[0];
   }
 
   /**
    * @description Returns the next value that would be use to redo or undo without modifying history.
    * @public
-   * @returns {Type | undefined} The next redo value.
+   * @returns {Value | undefined} The next redo value.
    */
-  public peekNext(): Type | undefined {
+  public peekNext(): Value | undefined {
     return super.history[super.history.length - 1];
   }
 
   /**
    * @description Takes the last value.
    * @public
-   * @returns {(Type | undefined)} 
+   * @returns {(Value | undefined)} 
    */
-  public take(): Type | undefined {
+  public take(): Value | undefined {
     return super.history.pop();
   }
 }
