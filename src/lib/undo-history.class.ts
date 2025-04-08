cdc5de6db85a7c232a4a93ba445ec3322b54ae9c
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-04-08 20:05:01 +0000

    refactor(UndoHistory): change the `Type` to `Value`, and `Storage` to `DataType`.

--- src/lib/undo-history.class.ts
+++ src/lib/undo-history.class.ts
@@ -10,13 +10,13 @@ import { HistoryAppend } from './history-append.abstract';
  * @description Manages the undo history with append mechanism.
  * @export
  * @class UndoHistory
- * @template [Type=any] The type of elements stored in the history.
+ * @template [Value=any] The type of elements stored in the history.
  * @template {number} [Size=number] The maximum size of the history.
- * @template {DataCore<Type[]>} [Storage=Data<Type[]>] 
- * @extends {HistoryAppend<Type, Size, Storage>}
+ * @template {DataCore<Value[]>} [DataType=Data<Value[]>] 
+ * @extends {HistoryAppend<Value, Size, DataType>}
  */
 export class UndoHistory<
-  Type = any,
+  Value = any,
   Size extends number = number,
-  Storage extends DataCore<Type[]> = Data<Type[]>
-> extends HistoryAppend<Type, Size, Storage>{};
+  DataType extends DataCore<Value[]> = Data<Value[]>
+> extends HistoryAppend<Value, Size, DataType>{};
