18aeaca2989f9c1112ce3caa4648685ac1d8cfd8
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-04-13 00:02:52 +0000

    test: update.

--- src/test/readme.md.spec.ts
+++ src/test/readme.md.spec.ts
@@ -1,9 +1,15 @@
 // Class.
+import { Data, DataCore } from "@typescript-package/data";
 import {
   History as AbstractHistory,
+  HistoryBase as AbstractHistoryBase,
+  HistoryCurrent as AbstractHistoryCurrent,
   HistoryAppend as AbstractHistoryAppend,
-  HistoryPrepend as AbstractHistoryPrepend
+  HistoryPrepend as AbstractHistoryPrepend,
+  HistoryStorage as AbstractHistoryStorage,
 } from "../lib";
+
+// History
 
 // Initialize.
 const history = new class History<Type, Size extends number = number> extends AbstractHistory<Type, Size>{}({value: 5, size: 5});
@@ -47,7 +53,7 @@ console.log(history.undoHistory.get(), history.current, history.redoHistory.get(
 
 console.groupEnd();
 
-
+// HistoryAppend
 
 console.group(`HistoryAppend README.md`);
 
@@ -69,7 +75,7 @@ console.log(historyAppend.get()); // [127]
 
 console.groupEnd();
 
-
+// historyPrepend
 
 console.group(`HistoryPrepend README.md`);
 
@@ -89,4 +95,92 @@ console.log(`peekNext()`, historyPrepend.first()); // Outputs: 227
 console.log(historyPrepend.take()); // 227
 console.log(historyPrepend.get()); // [327, 127]
 
+console.groupEnd();
+
+// HistoryStorage
+export class HistoryStorage<
+  Value,
+  DataType extends DataCore<Value[]> = Data<Value[]>
+> extends AbstractHistoryStorage<Value, DataType> {
+  public override set(value: Value[]) {
+    super.set(value);
+    return this;
+  }
+}
+
+console.group(`HistoryStorage README.md`);
+const historyStorage = new HistoryStorage(['a']);
+console.log(historyStorage.get()); // ['a']
+console.log(historyStorage.length); // 1
+console.log(historyStorage.isEmpty()); // false
+console.log(historyStorage.data.value); // ['a']
+historyStorage.set(['b']);
+console.log(historyStorage.get()); // ['b']
+historyStorage.set(['b', 'c', 'd']);
+console.log(historyStorage.get()); // ['b', 'c', 'd']
+console.log(historyStorage.length); // 3
+historyStorage.destroy();
+console.log(Object.hasOwn(historyStorage.data, 'value')); // false
+console.groupEnd();
+
+
+// HistoryCurrent
+
+export class HistoryCurrent<
+  Value,
+  DataType extends DataCore<Value[]> = Data<Value[]>
+> extends AbstractHistoryCurrent<Value, DataType> {
+
+  public override get value() {
+    return super.data.value[0]
+  }
+
+  public has() {
+    return super.data.value.length > 0;
+  }
+
+  public override set(value: Value[]) {
+    super.set(value);
+    return this;
+  }
+
+  public update(value: Value) {
+    super.set([value]);
+    return this;
+  }
+}
+
+console.group(`HistoryCurrent README.md`);
+const historyCurrent = new HistoryCurrent({value: 'a'});
+console.log(`get()`, historyCurrent.get()); // ['a']
+console.log(`length`, historyCurrent.length); // 1
+console.log(`isEmpty()`, historyCurrent.isEmpty()); // false
+console.log(`data.value`, historyCurrent.data.value); // ['a']
+historyCurrent.set(['b']);
+console.log(`get()`, historyCurrent.get()); // ['b']
+console.log(`length`, historyCurrent.length); // 1
+console.log(`isEmpty()`, historyCurrent.isEmpty()); // false
+historyCurrent.set(['b', 'c', 'd']);
+console.log(`get()`, historyCurrent.get()); // ['b', 'c', 'd']
+console.log(`length`, historyCurrent.length); // 3
+console.log(`isEmpty()`, historyCurrent.isEmpty()); // false
+historyCurrent.update('e');
+console.log(`get()`, historyCurrent.get()); // ['e']
+console.log(`length`, historyCurrent.length); // 1
+console.log(`isEmpty()`, historyCurrent.isEmpty()); // false
+historyCurrent.destroy();
+
+
+// HistoryBase
+
+export class HistoryBase<
+  Value,
+  Size extends number = number,
+  DataType extends DataCore<Value[]> = Data<Value[]>
+> extends AbstractHistoryBase<Value, Size, DataType> {}
+
+console.group(`HistoryBase README.md`);
+const historyBase = new HistoryBase({value: 'a', size: 5});
+console.log(`HistoryBase`, historyBase);
+console.log(`get()`, historyBase.get()); // ['a']
 console.groupEnd();
