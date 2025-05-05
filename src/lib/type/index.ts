9b2c45492303498112e5f52a48615cf444185e95
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-05-05 14:54:23 +0000

    chore(type/index): update.

--- src/lib/type/index.ts
+++ src/lib/type/index.ts
@@ -1,3 +1,2 @@
 export type { HistoryCurrentConstructor } from './history-current-constructor.type';
-export type { DataConstructor } from './data-constructor.type';
 export type { HistoryCoreConstructor } from './history-core-constructor.type';
