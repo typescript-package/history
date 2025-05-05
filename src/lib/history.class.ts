3993c6f38cd682ede63bca0e5893e62777d3d007
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-05-05 14:54:44 +0000

    refactor(`History`): make it type-safe customizable.

--- src/lib/history.class.ts
+++ src/lib/history.class.ts
@@ -2,30 +2,39 @@ import {
   // Class.
   Data,
   // Abstract.
-  DataCore
+  DataCore,
+  // Type.
+  DataConstructorInput
 } from '@typescript-package/data';
 // Abstract.
-import { HistoryBase } from './base/history-base.abstract';
+import { CurrentHistory, HistoryBase, RedoHistory, UndoHistory } from './base';
 /**
  * @description The class to manage the value changes.
  * @export
  * @class History
  * @template Value 
  * @template {number} [Size=number] 
- * @template {DataCore<Value[]>} [DataType=Data<Value[]>] 
+ * @template {DataCore<readonly Value[]>} [DataType=Data<readonly Value[]>] 
  */
 export class History<
   Value,
   Size extends number = number,
-  DataType extends DataCore<Value[]> = Data<Value[]>
-> extends HistoryBase<Value, Size, DataType> {
+  DataType extends DataCore<readonly Value[]> = Data<readonly Value[]>
+> extends HistoryBase<
+  Value,
+  Size,
+  DataType,
+  CurrentHistory<Value, DataType>,
+  RedoHistory<Value, Size, DataType>,
+  UndoHistory<Value, Size, DataType>
+> {
   /**
    * @description The max size for undo history.
    * @public
    * @static
    * @type {number}
    */
-  public static override size = Infinity;
+  public static override size: number = Infinity;
 
   /**
    * @description Returns the `string` tag representation of the `History` class when used in `Object.prototype.toString.call(instance)`.
@@ -35,5 +44,12 @@ export class History<
    */
   public override get [Symbol.toStringTag](): string {
     return History.name;
+  }
+
+  constructor(
+    {value, size}: { value?: Value, size?: Size} = {},
+    data?: DataConstructorInput<readonly Value[], DataType>,
+  ) {
+    super(arguments[0], data, { current: CurrentHistory, redo: RedoHistory, undo: UndoHistory });
   }
 }
