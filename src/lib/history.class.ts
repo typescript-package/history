db817103e70ca0e086219c52a8a4b9477ea236f3
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-05-06 15:21:29 +0000

    refactor(`History`): customize the type of current, redo and undo history.

--- src/lib/history.class.ts
+++ src/lib/history.class.ts
@@ -8,6 +8,9 @@ import {
 } from '@typescript-package/data';
 // Abstract.
 import { CurrentHistory, HistoryBase, RedoHistory, UndoHistory } from './base';
+import { HistoryCore, HistoryCurrent } from './core';
+// Type.
+import { HistoryCoreConstructor, HistoryCurrentConstructor } from './type';
 /**
  * @description The class to manage the value changes.
  * @export
@@ -19,15 +22,11 @@ import { CurrentHistory, HistoryBase, RedoHistory, UndoHistory } from './base';
 export class History<
   Value,
   Size extends number = number,
-  DataType extends DataCore<readonly Value[]> = Data<readonly Value[]>
-> extends HistoryBase<
-  Value,
-  Size,
-  DataType,
-  CurrentHistory<Value, DataType>,
-  RedoHistory<Value, Size, DataType>,
-  UndoHistory<Value, Size, DataType>
-> {
+  DataType extends DataCore<readonly Value[]> = Data<readonly Value[]>,
+  CurrentType extends HistoryCurrent<Value, DataType> = CurrentHistory<Value, DataType>,
+  RedoType extends HistoryCore<Value, Size, DataType> = RedoHistory<Value, Size, DataType>,
+  UndoType extends HistoryCore<Value, Size, DataType> = UndoHistory<Value, Size, DataType>,
+> extends HistoryBase<Value, Size, DataType, CurrentType, RedoType, UndoType> {
   /**
    * @description The max size for undo history.
    * @public
@@ -46,10 +45,35 @@ export class History<
     return History.name;
   }
 
+  /**
+   * Creates an instance of `History`.
+   * @constructor
+   * @param {{ value?: Value, size?: Size}} [param0={}] The optional `value` and maximum undo history `size`.
+   * @param {Value} param0.value The initial value.
+   * @param {Size} param0.size  The maximum undo history size.
+   * @param {?DataConstructorInput<readonly Value[], DataType>} [data] The custom data holder for history, current, undo and redo.
+   * @param {{
+   *       current?: HistoryCurrentConstructor<Value, DataType, CurrentType>,
+   *       redo?: HistoryCoreConstructor<Value, Size, DataType, RedoType>,
+   *       undo?: HistoryCoreConstructor<Value, Size, DataType, UndoType>,
+   *     }} [param1={}] 
+   * @param {HistoryCurrentConstructor<Value, DataType, CurrentType>} param1.current Custom current history class of `HistoryCurrent`.
+   * @param {HistoryCoreConstructor<Value, Size, DataType, RedoType>} param1.redo Custom redo history class of `HistoryCore`.
+   * @param {HistoryCoreConstructor<Value, Size, DataType, UndoType>} param1.undo Custom undo history class of `HistoryCore`.
+   */
   constructor(
     {value, size}: { value?: Value, size?: Size} = {},
     data?: DataConstructorInput<readonly Value[], DataType>,
+    {current, redo, undo}: {
+      current?: HistoryCurrentConstructor<Value, DataType, CurrentType>,
+      redo?: HistoryCoreConstructor<Value, Size, DataType, RedoType>,
+      undo?: HistoryCoreConstructor<Value, Size, DataType, UndoType>,
+    } = {},
   ) {
-    super(arguments[0], data, { current: CurrentHistory, redo: RedoHistory, undo: UndoHistory });
+    super(arguments[0], data, {
+      current: current ?? CurrentHistory as HistoryCurrentConstructor<Value, DataType, CurrentType>,
+      redo: redo ?? RedoHistory as unknown as HistoryCoreConstructor<Value, Size, DataType, RedoType>,
+      undo: undo ?? UndoHistory as unknown as HistoryCoreConstructor<Value, Size, DataType, UndoType>
+    });
   }
 }
